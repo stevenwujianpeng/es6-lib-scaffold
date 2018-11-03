d05f8c2ab4a0015a03bf0161ff27e798e9b1af22
吴建鹏 <jianpeng.wu@geely.com>
2018-11-03 13:08:20 +0800

    feat: 脚手架的命令行工具；

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,28 @@
 #! /usr/bin/env node
 
-var Git = require("nodegit");
+const fs = require('fs-extra');
+const path = require('path');
+const Git = require("nodegit");
+const program = require('commander');
+const templateGitUrl = 'https://github.com/stevenwujianpeng/js-min-template.git';
 
-Git.Clone("https://github.com/stevenwujianpeng/js-min-template.git", "js-min-template").then(function(repository) {
-    // Work with the repository object here.
-});
+program
+  .command('create <project>')
+  .action((project) => {
+    console.log('准备创建项目: ', project);
+    console.log('开始请求仓库...');
+
+    Git.Clone(templateGitUrl, project).then(function () {
+      // process.exit(1);
+      const gitPath = path.join(process.cwd(), project, '.git');
+
+      fs.remove(gitPath, function () {
+        console.log('创建成功');
+        console.log('cd %s', project);
+        console.log('npm i');
+      });
+    });
+  });
+
+program.parse(process.argv);
+
