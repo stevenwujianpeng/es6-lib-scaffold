9a47b4d207b61b4721260639593e97d8e0cd2414
吴建鹏 <jianpeng.wu@geely.com>
2018-11-04 12:01:07 +0800

    feat：增加对当前已经存在工程名的判断逻辑；

--- bin/index.js
+++ bin/index.js
@@ -9,8 +9,15 @@ const templateGitUrl = 'https://github.com/stevenwujianpeng/js-min-template.git'
 program
   .command('create <project>')
   .action((project) => {
-    console.log('准备创建项目: ', project);
-    console.log('开始请求仓库...');
+    const rootDir = process.cwd();
+    const projectDir = path.join(rootDir, project);
+
+    if (fs.pathExistsSync(projectDir)) {
+      console.log('已经存在当前目录！请选择新的项目名！');
+    } else {
+      console.log('准备创建项目: ', project);
+      console.log('开始请求仓库...');
+    }
 
     Git.Clone(templateGitUrl, project).then(function () {
       // process.exit(1);
